21cc5ff691bca24e2e94747dfa773391849f4593
Daesun Hwang <hds650588@gmail.com>
2022-10-04 00:25:49 +0900

    HttpSession이용하여 로그인 정보 기억하는 코드 작성

--- src/main/java/study/springboot/controller/LoginController.java
+++ src/main/java/study/springboot/controller/LoginController.java
@@ -1,11 +1,15 @@
 package study.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.springboot.domain.member.Member;
 import study.springboot.service.web.LoginService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -22,17 +26,29 @@ public class LoginController {
     }
 
     @PostMapping(value="/login")
-    public String loginMember(@ModelAttribute LoginForm loginForm){
+    public String loginMember(@ModelAttribute LoginForm loginForm, HttpServletRequest request, Model model){
         Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getLoginPassword());
-        System.out.println("loginMemberId = " + loginForm.getLoginId() + "loginMemberPassword =" + loginForm.getLoginPassword());
+
         if(loginMember == null){
             return "members/login";
         }
+
+        //로그인 성공하면, 세션에 생성
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConstants.LOGIN_MEMBER, loginMember);
+
+        model.addAttribute("member",loginMember);
+
         return "boards/main";
     }
 
-    @GetMapping(value = "/logout")
-    public String logout(){
-        return "/";
+    @PostMapping(value = "/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        // create값을 false로 하면 원래 세션이 있으면 그 세션 반환, 세션 없으면 새로 생성 안함
+        if(session != null){
+            session.invalidate(); // 세션 날림
+        }
+        return "home";
     }
 }
---
+++ src/main/java/study/springboot/controller/SessionConstants.java
@@ -0,0 +1,7 @@
+package study.springboot.controller;
+
+public interface SessionConstants {
+
+    String LOGIN_MEMBER = "loginMember";
+
+}
